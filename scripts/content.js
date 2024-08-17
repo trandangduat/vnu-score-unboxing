5dadee3d914ae0d1df0ddbfaec3fd481cd5cad00
Tran Dang Duat <23020023@vnu.edu.vn>
2024-08-17 15:26:39 +0700

    Hide subject's info with "unlock" buttons

--- scripts/content.js
+++ scripts/content.js
@@ -4,15 +4,32 @@ main = async () => {
             const iframe1 = document.querySelector('iframe');
             iframe1.addEventListener("load", () => {
                 const iframe2 = iframe1.contentWindow.document.querySelector('iframe');
+
                 iframe2.addEventListener("load", () => {
                     const subjects = iframe2.contentWindow.document.querySelectorAll('#divList3 > table > tbody tr');
+
                     subjects.forEach((subject) => {
-                        const subjectInfo = subject.querySelectorAll('td');
-                        if (subjectInfo.length < 7)
+                        const subjectInfoDOM = subject.querySelectorAll('td');
+                        const subjectInfo = [];
+
+                        if (subjectInfoDOM.length < 7)
                             return;
 
+                        for (let i = 0; i < subjectInfoDOM.length; i++) {
+                            subjectInfo.push(subjectInfoDOM[i].innerHTML);
+                        }
+
                         for (let i = 4; i < 8; i++) {
-                            subjectInfo[i].style.backgroundColor = "yellow";
+                            subjectInfoDOM[i].style.backgroundColor = "#f0f0f0";
+                            subjectInfoDOM[i].innerHTML = `<p style="cursor:pointer;">🔑 unlock</p>`;
+                        }
+                        for (let i = 4; i < 8; i++) {
+                            subjectInfoDOM[i].addEventListener("click", () => {
+                                for (let j = 4; j < 8; j++) {
+                                    subjectInfoDOM[j].style.backgroundColor = "white";
+                                    subjectInfoDOM[j].innerHTML = subjectInfo[j];
+                                }
+                            });
                         }
 
                         // const subjectName = subjectInfo[1].innerText;
